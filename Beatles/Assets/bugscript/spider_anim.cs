a9d8225d7b0d5224df94318569b1eeb1987d5b14
18b26 <1>
2019-10-09 01:27:30 +0900

    虫挙動作成中

---
+++ Beatles/Assets/bugscript/spider_anim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spider_anim : MonoBehaviour
+{
+    Animation anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = this.gameObject.GetComponent<Animation>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        anim.Play();
+    }
+}
