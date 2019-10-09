4929f080e759ba6fad90238facd8661a44081312
18b26 <1>
2019-10-09 13:34:40 +0900

    虫挙動作成中

---
+++ Beatles/Assets/bugscript/ObjectController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class ObjectController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform m_target = null;
+
+    private NavMeshAgent m_navAgent = null;
+
+    private void Awake()
+    {
+        m_navAgent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Start()
+    {
+        if (m_target != null)
+        {
+            m_navAgent.destination = m_target.position;
+        }
+    }
+
+} // class ObjectController
