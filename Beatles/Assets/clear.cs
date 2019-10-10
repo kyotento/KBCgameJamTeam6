208ac571c7757e951adb7f216f5fb83268861f62
拠点斗 <kyotento0507@gmail.com>
2019-10-10 16:46:58 +0900

    gy

--- Beatles/Assets/clear.cs
+++ Beatles/Assets/clear.cs
@@ -2,21 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class clear : MonoBehaviour
 {
 
     [SerializeField] GameObject ooo;
     // Start is called before the first frame update
     void Start()
     {
-        
+   
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-     
-        
+        //タイマー検索。
+        var timer = ooo.GetComponent<Timer>().time;
+        //失敗条件。
+        //もし残り時間が０になった時。
+        if (timer <= 0)
+        {
+
+        }
+
     }
 }
