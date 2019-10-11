7a744fc15b2efae519e838cc53a7a598cc617d49
18b26 <1>
2019-10-11 14:19:49 +0900

    bug

---
+++ Beatles/Assets/bugscript/bug_management.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bug_management : MonoBehaviour
+{
+    public bool deathflag = false;
+    public bool isStop = false;
+    GameObject obj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GameObject.Find("Sphere (1)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+}
